ecf8975385df34e103f2b7847bb873ed28028037
Lance <lance_amethyst@hotmail.com>
2016-03-07 18:37:30 +0800

    re deploy

--- dist/dom/ix.js
+++ dist/dom/ix.js
@@ -1,7 +1,7 @@
 /*
  * IX project 
  * https://github.com/lance-amethyst/IX
- * Distrib No : 20160307T154221Z464
+ * Distrib No : 20160307T180933Z608
  *
  * Copyright (c) 2015 Lance GE, contributors
  * Licensed under the MIT license.
