9ad3014ff30c43ec9b94e63f590177798028ec75
Lance <lance_amethyst@hotmail.com>
2016-09-30 12:27:55 +0800

    update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,79 +1,14 @@
 require("./base/ix.js");
 var fs = require('fs');
+var ixCfg = require('./config.js');
 
 module.exports = function (grunt) {
-	function getFilePaths(filenames, basedir){
-		return IX.map(filenames.split(","), function(fname){return basedir + "/" + fname + ".js";});
-	}
-	var commonJsFiles = getFilePaths("ix,array,condition,ds,math,date,misc,tpl,task", "base");
-	var domJsFiles = getFilePaths("ix,net,misc", "dom");
-	var nodeJsFiles =  getFilePaths("ix", "node");
-
-	var hdrStr = fs.readFileSync("base/hdr.js").toString();
+	var hdrStr = fs.readFileSync("./base/hdr.js").toString();
 	fs.writeFileSync("dist/hdr.js", hdrStr.replace("DATE", IX.getTimeStrInMS()));
 
-	grunt.initConfig({
-		pkg : grunt.file.readJSON("package.json"),
-		concat: {
-			dom: {
-				"src": ["dist/hdr.js", "dom/hdr.js"].concat(commonJsFiles, domJsFiles),
-				"dest": 'dist/dom/ix.js'
-			},
-			node : {
-				"src": ["dist/hdr.js", "node/hdr.js"].concat(commonJsFiles, nodeJsFiles),
-				"dest": 'dist/node/ix.js'
-			}
-		},
-		jshint : {
-			options :{
-				//curly:true,  //大括号包裹  
-				//eqeqeq:true,  //对于简单类型，使用===和!==，而不是==和!=  
-				//newcap:true,  //对于首字母大写的函数（声明的类），强制使用new  
-				noarg:true,  //禁用arguments.caller和arguments.callee  
-				//sub:true,  //对于属性使用aaa.bbb而不是aaa['bbb']  
-				undef:true,  //查找所有未定义变量  
-				boss:true,//查找类似与if(a = 0)这样的代码  
-				node:true,
-				globals: {
-					IX : true,
-					window: true,
-					document : true,
-					IX_GLOBAL : true,
-					IXDebug : true,
-					debugIsAllow : true,
-					IX_DEBUG_MODE :true,
-					IX_SCRIPT_NAME : true,
-					IX_VERSION : true,
-					"$X" : true,
-					"$XA" : true,
-					"$XD" : true,
-					"$XP" : true,
-					"$XE" : true,
-					"$XF" : true
-				}
-			},
-			files : {
-				src : ['base/*.js', 'dom/*.js', 'node/*.js']
-			},
-			afterconcat: ['dist/*/ix.js']
-		},
-		uglify : {
-			options: {  
-				banner:' /*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd hh:MM:ss") %> */\n',
-				beautify: {ascii_only:true},
-				maxLineLen : 8192
-			},
-			
-			dom :{
-				src:'dist/dom/ix.js',  
-				dest:'dist/dom/ix.min.js'  
-			},
-			node :{  
-				src:'dist/node/ix.js',  
-				dest:'dist/node/ix.min.js'  
-			}
-		}
-	});
+	grunt.initConfig(IX.inherit({
+		pkg : grunt.file.readJSON("./package.json")
+	}, ixCfg));
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
--- testcase/run.js
+++ testcase/run.js
@@ -1,14 +1,3 @@
 var testTask = require("./test.js");
 
-
 testTask();
-
-
-
-
-
-
-
-
-
-
